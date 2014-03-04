2b56bb1b709cd841e4a827a7b47a466fa9c606dd
anymoto <ana@castromartinez.com>
2014-03-04 11:00:20 -0600

    Add missing expectation

--- lib/crackle_pop.rb
+++ lib/crackle_pop.rb
@@ -1,17 +1,17 @@
 require "crackle_pop/version"
 
 module CracklePop
-  def run
-    100.times do |num|
-      puts cracklepop num+1
-    end
+  def get_list
+    list = []
+    100.times { |num| list << cracklepop(num+1) }
+    list
   end
 
   def cracklepop(number)
     case
+    when number % 15 == 0 then "CracklePop"
     when number % 3 == 0 then "Crackle"
     when number % 5 == 0 then "Pop"
-    when number % 15 == 0 then "CracklePop"
     else number
     end
   end
--- spec/crackle_pop_spec.rb
+++ spec/crackle_pop_spec.rb
@@ -3,19 +3,23 @@ require 'crackle_pop'
 describe CracklePop do
   let(:dummy_class) { Class.new { extend CracklePop } }
 
-  describe '#run' do
+  describe '#get_list' do
     it 'should prints out a list of numbers replacing with Crackle and Pop' do
-      expect(dummy_class.run.to_s).not_to be_empty
+      expect(dummy_class.get_list.take(6)).to match_array([1,2,'Crackle',4,'Pop','Crackle'])
     end
   end
 
   describe '#cracklepop' do
     it 'should print Crackle if number is divisible by 3' do
-      expect(dummy_class.cracklepop(12)).to eq "Crackle"
+      expect(dummy_class.cracklepop(12)).to eq 'Crackle'
     end
 
     it 'should print Pop if number is divisible by 5' do
-      expect(dummy_class.cracklepop(20)).to eq "Pop"
+      expect(dummy_class.cracklepop(20)).to eq 'Pop'
+    end
+
+    it 'should print CracklePop if number is divisible by 3 and 5' do
+      expect(dummy_class.cracklepop(45)).to eq 'CracklePop'
     end
   end
 end
